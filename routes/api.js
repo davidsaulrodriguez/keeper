e217230670f1bef74a2d9984e2fd9f60c9be9f4e
David Rodriguez <david@bsdadm.com>
2021-01-23 10:05:57 -0500

    add the DELETE logic

--- routes/api.js
+++ routes/api.js
@@ -7,10 +7,47 @@ module.exports = (app) => {
   });
 
   app.post('/api/notes', (req, res) => {
-      // Post Magic
+    let newNote = req.body;
+
+    //assign an ID to each new note
+    newNote.id = savedNotes.length.toString();
+    
+    //push new note into the db JSON file
+    savedNotes.push(newNote);
+
+    // Write to the JSON file to update with new data
+    fs.writeFile("./public/assets/db/db.json", JSON.stringify(savedNotes),(err) => {
+      if (err) {
+        console.log(err); 
+        res.sendStatus(404);
+      } 
+      else { 
+        console.log("New Note Saved!");
+        res.sendStatus(200);
+      }
+    });
   });
 
   app.delete('/api/notes/:id', (req, res) => {
-      // Delete Magic
+      //grab query params
+    let id = req.params.id;
+
+    //finds element at ID
+    let theID = (element) => element.id === id;
+    let removeNote = savedNotes.findIndex(theID);
+
+    //delete the element from the array
+    savedNotes.splice(removeNote, 1);
+    
+    // Write a JSON file so update with new data
+    fs.writeFile("./public/assets/db/db.json", JSON.stringify(savedNotes),(err) => {
+      if (err) {
+        console.log(err); 
+        res.sendStatus(404);
+      } else { 
+        console.log("Note Deleted!");
+        res.sendStatus(200);
+      }
+    });
   });
 }
